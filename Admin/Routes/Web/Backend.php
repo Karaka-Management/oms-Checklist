ea695d2639aae5d73a45156972766d49b8889c7e
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-08-31 22:30:36 +0200

    php cs autofix

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -8,45 +8,45 @@ use phpOMS\Router\RouteVerb;
 return [
     '^.*/checklist/list.*$' => [
         [
-            'dest' => '\Modules\Checklist\Controller\BackendController:viewChecklistList',
-            'verb' => RouteVerb::GET,
+            'dest'       => '\Modules\Checklist\Controller\BackendController:viewChecklistList',
+            'verb'       => RouteVerb::GET,
             'permission' => [
                 'module' => BackendController::MODULE_NAME,
-                'type'  => PermissionType::READ,
-                'state' => PermissionState::CHECKLIST,
+                'type'   => PermissionType::READ,
+                'state'  => PermissionState::CHECKLIST,
             ],
         ],
     ],
     '^.*/checklist/template/list.*$' => [
         [
-            'dest' => '\Modules\Checklist\Controller\BackendController:viewChecklistTemplateList',
-            'verb' => RouteVerb::GET,
+            'dest'       => '\Modules\Checklist\Controller\BackendController:viewChecklistTemplateList',
+            'verb'       => RouteVerb::GET,
             'permission' => [
                 'module' => BackendController::MODULE_NAME,
-                'type'  => PermissionType::READ,
-                'state' => PermissionState::TEMPLATE,
+                'type'   => PermissionType::READ,
+                'state'  => PermissionState::TEMPLATE,
             ],
         ],
     ],
     '^.*/checklist/template/create.*$' => [
         [
-            'dest' => '\Modules\Checklist\Controller\BackendController:viewChecklistTemplateCreate',
-            'verb' => RouteVerb::GET,
+            'dest'       => '\Modules\Checklist\Controller\BackendController:viewChecklistTemplateCreate',
+            'verb'       => RouteVerb::GET,
             'permission' => [
                 'module' => BackendController::MODULE_NAME,
-                'type'  => PermissionType::CREATE,
-                'state' => PermissionState::TEMPLATE,
+                'type'   => PermissionType::CREATE,
+                'state'  => PermissionState::TEMPLATE,
             ],
         ],
     ],
     '^.*/checklist/template/view.*$' => [
         [
-            'dest' => '\Modules\Checklist\Controller\BackendController:viewChecklistTemplateView',
-            'verb' => RouteVerb::GET,
+            'dest'       => '\Modules\Checklist\Controller\BackendController:viewChecklistTemplateView',
+            'verb'       => RouteVerb::GET,
             'permission' => [
                 'module' => BackendController::MODULE_NAME,
-                'type'  => PermissionType::READ,
-                'state' => PermissionState::TEMPLATE,
+                'type'   => PermissionType::READ,
+                'state'  => PermissionState::TEMPLATE,
             ],
         ],
     ],
