e69cc42f5e40192bdfeddbb11e93fff69998f540
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:07 +0200

    fix version and bugs

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\Checklist
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -30,7 +30,7 @@ use phpOMS\Message\ResponseAbstract;
  * Checklist api controller class.
  *
  * @package Modules\Checklist
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
